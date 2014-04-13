805c48275b01259fb452fbc2a68c05fe4f68921f
Kwanghoon <lazyswamp@gmail.com>
2014-04-13 17:35:57 +0900

    Added printing fingering results.

--- Main.hs
+++ Main.hs
@@ -7,5 +7,22 @@ import Sonata_Fortea
 
 readScore l = [scaleOctavesWithStrings f s | (f,s) <- l]
 
-main = putStrLn $ show $ readScore sonata_fortea -- lagrima_guitar
+printFingering score = pr fingering
+  where
+    fingering = readScore score
+    
+    pr [] = putStrLn ""
+    pr (alts:f) = do prAlts alts; pr f
+                     
+    prAlts [] = putStrLn ""
+    prAlts (alt:alts) = do prAlt alt; putStr "; "; prAlts alts
+                           
+    prAlt [] = return ()
+    prAlt [(s,f)] = prSF s f False
+    prAlt ((s,f):alt) = do prSF s f True; prAlt alt
+                           
+    prSF s f b = putStr (show f ++ "f " ++ show s ++ "s" ++ postfix)
+                 where postfix = if b then ", " else ""
+
+main = printFingering sonata_fortea -- sonata_fortea -- lagrima_guitar
 
